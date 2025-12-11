d87817b0149c2029f5777930464a6f910c0ef34b
Oscar Serrano Yanez <2691820@tcs.com>
2025-12-11 14:05:06 -0600

    add useEffect and api pokemon

--- app/(pages)/pokemon/page.tsx
+++ app/(pages)/pokemon/page.tsx
@@ -1,22 +1,38 @@
 'use client'
+import type { Pokemon } from "@/app/interfaces/pokemon.model"
 import axios from "axios"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import './pokemon.style.scss'
 
 function Pokemon() {
 
-    const [pokemon, setPokemon] = useState()
-    
-    const getPokemons = async () => {
-        try {
-            let query = axios.get('https://pokeapi.co/api/v2/pokemon/ditto')
-        } catch (error) {
-            console.log(`El error es: ${error}`)
-        }
+  const [pokemon, setPokemon] = useState<Pokemon>()
+
+  const getPokemons = async (namePokemon: string) => {
+    try {
+      let query = await axios.get(`https://pokeapi.co/api/v2/pokemon/${namePokemon}`)
+      setPokemon(query.data)
+      console.log(query.data)
+      return query.data
+    } catch (error) {
+      console.log(`El error es: ${error}`)
     }
-    
+  }
+
+  useEffect(() => {
+    getPokemons("articuno")
+  }, [])
 
   return (
-    <div>Pokemon</div>
+    <div>
+      {
+        pokemon ? (<>
+          <img src={pokemon.sprites.front_default} alt="Image de pokemon" className="img__pokemon"/>
+          <h1>{pokemon?.name.toUpperCase()}</h1>
+        </>) : <h3>No existe el pokemon</h3>
+      }
+
+    </div>
   )
 }
 
